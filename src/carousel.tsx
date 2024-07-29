d12d93c1b0bb653e459193d850585f3792cbf4e5
Solo <juanoliugomezplatero@gmail.com>
2024-07-28 23:46:43 -0300

    fixes and images replacing

--- src/carousel.tsx
+++ src/carousel.tsx
@@ -6,25 +6,25 @@ export const Carousel: React.FC = () => {
       <div className="slider">
         <a href="#img_1">
           <img
-            src="https://images.pexels.com/photos/1303502/pexels-photo-1303502.jpeg"
+            src="https://images.pexels.com/photos/50594/sea-bay-waterfront-beach-50594.jpeg?auto=compress&cs=tinysrgb&w=800"
             alt="Slide 1"
           />
         </a>
         <a href="#img_2">
           <img
-            src="https://images.pexels.com/photos/1303502/pexels-photo-1303502.jpeg"
+            src="https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg?auto=compress&cs=tinysrgb&w=800"
             alt="Slide 2"
           />
         </a>
         <a href="#img_3">
           <img
-            src="https://images.pexels.com/photos/1303502/pexels-photo-1303502.jpeg"
+            src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=800"
             alt="Slide 3"
           />
         </a>
         <a href="#img_4">
           <img
-            src="https://images.pexels.com/photos/1303502/pexels-photo-1303502.jpeg"
+            src="https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&w=800"
             alt="Slide 4"
           />
         </a>
